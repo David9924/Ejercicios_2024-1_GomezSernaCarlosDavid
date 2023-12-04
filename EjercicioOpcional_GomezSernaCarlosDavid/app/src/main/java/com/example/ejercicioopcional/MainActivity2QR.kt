334a38494c77486e8aca63f314c7bc7205474aca
David9924 <codigocero8@gmail.com>
2023-12-04 03:46:19 -0600

    Primer commit ejercicio opcional

---
+++ EjercicioOpcional_GomezSernaCarlosDavid/app/src/main/java/com/example/ejercicioopcional/MainActivity2QR.kt
@@ -0,0 +1,11 @@
+package com.example.ejercicioopcional
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2QR : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity2_qr)
+    }
+}
---
+++ EjercicioOpcional_GomezSernaCarlosDavid/app/src/main/java/com/example/ejercicioopcional/MainActivityInicio.kt
@@ -0,0 +1,11 @@
+package com.example.ejercicioopcional
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityInicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_inicio)
+    }
+}
